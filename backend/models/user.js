d17b014b1e46908ed0eb5b33bd4c2b360ae2d3bb
Aftab <muhammadaftab584@gmail.com>
2025-11-04 09:39:05 +0500

    db connect

---
+++ backend/models/user.js
@@ -0,0 +1,7 @@
+import mongoose, {Schema} from 'mongodb';
+
+const userSchema = Schema({
+    'username': {
+        
+    }
+})
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+const app = express();
+
+dotenv.config();
+app.on("error", (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err);
+});
+
+const PORT = process.env.PORT || 5000;
+(async function () {
+    try {
+        const db_connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(db_connection.connection.host, "DB Connected");
+        app.listen(PORT,() => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Error Connecting DB: ", error);
+        process.exit(1);
+    }
+})();
